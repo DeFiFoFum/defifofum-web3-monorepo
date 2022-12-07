8bb9388b5d7f30d9e8dc2099632456ff3d555fa5
defifofum <defifofum0@gmail.com>
2022-12-07 18:25:57 -0500

    feat: Provide examples

--- packages/safe-tx-builder/README.md
+++ packages/safe-tx-builder/README.md
@@ -1,2 +1,22 @@
 # Safe Tx Builder
 Use this package to create batch transactions on https://app.safe.global/ 
+
+## Example Usage
+The output of this can be imported into Safe's TX builder
+
+```typescript
+import { miniApeV2Builders } from "@defifofum/safe-tx-builder"
+import { MINI_APE_ADDRESS } from "./constants"
+
+const txBuilder = miniApeV2Builders.generateTxs_MiniApe_Set(
+    MINI_APE_ADDRESS,
+    [
+        {
+            pid: '0',
+            allocPoint: '0',
+        }
+    ]
+)
+
+console.log(JSON.stringify(txBuilder));
+```
--- packages/safe-tx-builder/examples/miniApeSet.ts
+++ packages/safe-tx-builder/examples/miniApeSet.ts
@@ -1,20 +1,21 @@
 
 // import { miniApeV2Builders } from "@defifofum/safe-tx-builder"
 import { miniApeV2Builders } from "../lib/safe-tx-builder"
-import { readJSONFile, writeJSONToFile } from "@defifofum/files"
 
 const MINI_APE_ADDRESS = '0x54aff400858Dcac39797a81894D9920f16972D1D';
 
 async function script() {
-    const file = await readJSONFile(__dirname + "/input.json")
-    
-    console.dir({file})
     const txBuilder = miniApeV2Builders.generateTxs_MiniApe_Set(
         MINI_APE_ADDRESS,
-        file as any,
+        [
+            {
+                pid: '0',
+                allocPoint: '0',
+            }
+        ]
     )
 
-    await writeJSONToFile(__dirname + "/SetTxBuilder.json", txBuilder, true)
+    console.log(JSON.stringify(txBuilder));
 }
 
 (async function () {
