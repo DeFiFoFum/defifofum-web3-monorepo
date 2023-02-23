a216e948d682395ef867813d08bdef2b46f2aa70
defifofum <defifofum0@gmail.com>
2023-02-23 17:30:29 -0500

    refactor: Improve demo

--- packages/etherscan-sdk/examples/logs.ts
+++ packages/etherscan-sdk/examples/logs.ts
@@ -1,4 +1,5 @@
-import EtherscanService from "../lib/etherscan/etherscan.service";
+// import { EtherscanService, Log } from "@defifofum/etherscan-sdk";
+import { EtherscanService, Log } from "../lib/etherscan";
 import { getConfig } from "../lib/config";
 
 const { baseUrl: BASE_URL, apiKey: API_KEY } = getConfig("bsc");
@@ -6,7 +7,7 @@ const { baseUrl: BASE_URL, apiKey: API_KEY } = getConfig("bsc");
 (async function () {
   const etherscanService = new EtherscanService(BASE_URL, API_KEY);
   const DEPOSIT_TOPIC = '0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c'
-  const logs = await etherscanService.getLogs(
+  const logs: Log[] = await etherscanService.getLogs(
     '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // WBNB
     DEPOSIT_TOPIC,
     {
